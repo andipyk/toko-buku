3377afbf17001706f4206d27ce8d3df3ddff384a
Andi Ganteng <andisyafrianda@gmail.com>
2019-03-10 20:58:19 +0700

    adding status column in users view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,14 +7,14 @@ User List
 @section('content')
 
 @if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
-    </div>
-@endif 
+<div class="alert alert-success">
+    {{ session('status') }}
+</div>
+@endif
 
 <div class="row">
     <div class="col-md-6">
-        <form action="{{ route('users.index') }}" > {{-- Jangan Pakai method Post --}}
+        <form action="{{ route('users.index') }}"> {{-- Jangan Pakai method Post --}}
             @csrf
             <div class="input-group mb-3">
                 <input type="text" name="keyword" id="keyword" value="{{ Request::get('keyword') }}" class="form-control col-md-10">
@@ -38,6 +38,7 @@ User List
             <th><b>Name</b></th>
             <th><b>Username</b></th>
             <th><b>Email</b></th>
+            <th><b>Status</b></th>
             <th><b>Avatar</b></th>
             <th><b>Action</b></th>
         </tr>
@@ -48,23 +49,36 @@ User List
             <td>{{ $user->name }}</td>
             <td>{{ $user->username }}</td>
             <td>{{ $user->email }}</td>
+            <td>
+                @if ($user->status == 'ACTIVE')
+                <span class="badge badge-success">
+                    {{ $user->status }}
+                </span>
+                @else
+                <span class="badge badge-danger">
+                    Belum Verifikasi
+                    {{--  {{ $user->status }}  --}}
+                </span>
+                @endif
+            </td>
             <td>@if($user->avatar)
                 <img src="{{ asset('storage/'.$user->avatar) }}" width="70px" />
                 @else
-                    N/A
+                N/A
                 @endif
             </td>
             <td>
                 <a href="{{ route('users.edit', ['id' => $user->id]) }}" class="btn btn-info text-white btn-sm">Edit</a>
-                <form action="{{ route('users.destroy', ['id' => $user->id ]) }}" class="d-inline" method="post" onsubmit="return confirm('Delete this user permanently?')">
+                <form action="{{ route('users.destroy', ['id' => $user->id ]) }}" class="d-inline" method="post"
+                    onsubmit="return confirm('Delete this user permanently?')">
                     @csrf
                     <input type="hidden" name="_method" value="DELETE">
                     <input type="submit" value="Delete" class="btn btn-danger btn-sm">
                 </form>
                 <a href="{{ route('users.show', ['id' => $user->id]) }}" class="btn btn-primary btn-sm">Detail</a>
             </td>
-            
-            {{--  <td>[TODO: actions]</td>  --}}
+
+            {{-- <td>[TODO: actions]</td> --}}
         </tr>
         @endforeach
     </tbody>
