551b080792a6a8bf4d4c649cfbca8c58fa2331db
Andi Ganteng <andisyafrianda@gmail.com>
2019-03-12 21:41:23 +0700

    add filter category

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -5,7 +5,25 @@ TODO title index cateogry
 @endsection
 
 @section('content')
-<a href="{{ route('categories.create') }}" class="btn btn-primary">create</a>
+<div class="row">
+    <div class="col-md-6">
+        <form action="{{ route('categories.index') }}">
+            <div class="input-group">
+                <input type="text" name="name" class="form-control" placeholder="Filter by category name">
+
+                <div class="input-group-append">
+                    <input type="submit" value="Filter" class="btn btn-primary">
+                    &nbsp;
+                    <a href="{{ route('categories.create') }}" class="btn btn-primary">create</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<hr class="my-3">
+
+
 <div class="row">
     <div class="col-md-12">
         <table class="table table-bordered table-stripped">
@@ -29,7 +47,7 @@ TODO title index cateogry
                         No Image
                         @endif
                     </td>
-                    <td>[ TODO action for category ]</td>
+                    <td><a href="{{ route('categories.edit', ['id' => $category->id]) }}" class="btn btn-info btn-sm">Edit</a></td>
                 </tr>
                 @endforeach
             </tbody>
